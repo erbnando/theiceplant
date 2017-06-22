26bd4059084f723982685d077e3d93b3870d9f6f
Nando Ramirez <erbnando@gmail.com>
2017-06-21 22:05:35 -0400

    added 404 template

---
+++ wp-content/themes/theiceplant/404.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="main">
+	<p class="textNormal">Page not found. Check out our <a href="<?php echo get_permalink( woocommerce_get_page_id('shop')); ?>">catalog</a>.</p>
+</div>
+
+<?php 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); ?>
+		<div class="main">
+			<?php the_content(); ?>
+		</div>
+		<?php
+	}
+}
+?>
+
+<?php get_footer();
