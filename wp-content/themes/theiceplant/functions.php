854375c9c3e419a73683bd052b6f4cc6a6591501
Nando Ramirez <erbnando@gmail.com>
2017-09-06 23:55:59 -0700

    another small update to images

--- wp-content/themes/theiceplant/functions.php
+++ wp-content/themes/theiceplant/functions.php
@@ -270,4 +270,30 @@ function woocommerce_header_add_to_cart_fragment( $fragments ) {
 
 add_image_size( 'book-cover', 350, 386 );
 
+/**
+ * Disable responsive image support (test!)
+ */
+
+// Clean the up the image from wp_get_attachment_image()
+add_filter( 'wp_get_attachment_image_attributes', function( $attr )
+{
+    if( isset( $attr['sizes'] ) )
+        unset( $attr['sizes'] );
+
+    if( isset( $attr['srcset'] ) )
+        unset( $attr['srcset'] );
+
+    return $attr;
+
+ }, PHP_INT_MAX );
+
+// Override the calculated image sizes
+add_filter( 'wp_calculate_image_sizes', '__return_false',  PHP_INT_MAX );
+
+// Override the calculated image sources
+add_filter( 'wp_calculate_image_srcset', '__return_false', PHP_INT_MAX );
+
+// Remove the reponsive stuff from the content
+remove_filter( 'the_content', 'wp_make_content_images_responsive' );
+
 ?>
